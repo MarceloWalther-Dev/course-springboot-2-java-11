32cbcb80f7c6e9530b71b1a8fa8af13df64949e5
Marcelo Walther da Silva <marcelo.walther.10@gmail.com>
2021-02-26 17:15:09 -0300

    Order, Instant, ISO 8601

---
+++ src/main/java/com/udemy/course/service/OrderService.java
@@ -0,0 +1,29 @@
+package com.udemy.course.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.udemy.course.entities.Order;
+import com.udemy.course.repositories.OrderRepository;
+
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	
+	public List<Order> findAll(){
+		return repository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> order = repository.findById(id);
+		return order.get();
+	}
+	
+}
