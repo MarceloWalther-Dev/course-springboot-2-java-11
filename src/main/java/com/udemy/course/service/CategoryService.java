187a4d85eec6c8caa7206e9099e2008ade5c2196
Marcelo Walther da Silva <marcelo.walther.10@gmail.com>
2021-03-01 10:39:32 -0300

    Created entity Category and resources

---
+++ src/main/java/com/udemy/course/service/CategoryService.java
@@ -0,0 +1,29 @@
+package com.udemy.course.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.udemy.course.entities.Category;
+import com.udemy.course.repositories.CategoryRepository;
+
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> category = repository.findById(id);
+		return category.get();
+	}
+	
+}
